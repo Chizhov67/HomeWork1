b856a4b2d954950b48c44544f88c5a227c8451bb
Andrey <chizhov67@gmail.com>
2023-01-15 19:08:50 +0300

    Изменен HomeWork1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int a, b;
+Console.Write("Введите a: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+				Console.WriteLine("max " + a + " min " + b);
+			}
+ else if (a < b)
+ {
+				Console.WriteLine("max " + b + " min " + a);
+			}
+
+ Console.ReadLine();                      
